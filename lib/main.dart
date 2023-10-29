879fcf4912805024c99bf4df45309690422b5e7c
AntonMoldakov <anton.moldakov@gmail.com>
2023-10-29 18:49:16 +0500

    feat: add bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:goal_diary/app.dart';
 import 'package:goal_diary/shared/services/api.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 import 'package:talker_dio_logger/talker_dio_logger.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   final talker = TalkerFlutter.init();
@@ -17,5 +21,18 @@ void main() {
   ));
   GetIt.I.registerSingleton(api);
 
-  runApp(MyApp());
+  Bloc.observer = TalkerBlocObserver(
+    talker: talker,
+    settings: const TalkerBlocLoggerSettings(
+      printStateFullData: false,
+      printEventFullData: false,
+    ),
+  );
+
+  FlutterError.onError =
+      (details) => GetIt.I<Talker>().handle(details.exception, details.stack);
+
+  runZonedGuarded(() => runApp(const MyApp()), (e, st) {
+    GetIt.I<Talker>().handle(e, st);
+  });
 }
